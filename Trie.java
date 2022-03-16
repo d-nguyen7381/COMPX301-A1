cb454034b23e16ddfb895fb9947459dd6c7dab95
d-nguyen7381 <d.nguyen7381@gmail.com>
2022-03-16 16:44:49 +1300

    Trie class (kinda?) done.

--- Trie.java
+++ Trie.java
@@ -3,17 +3,15 @@ public class Trie {
     private int newPhrase;
     private Node root;
     private Node currNode;
-    
 
     public Trie() {
-        newPhrase = hexDigits.length;
+        newPhrase = hexDigits.length + 1;
         root = new Node('\0', 0);
+        currNode = root;
 
-        for(int i = 0; i < hexDigits.length; i++) {
+        for(int i = 0; i < hexDigits.length; i++) { // loop that initializes the children of the root node
             root.children[i] = new Node(hexDigits[i], i);
         }
-
-        currNode = root;
     }
 
     class Node {
@@ -26,19 +24,47 @@ public class Trie {
             this.phrase = p;
         }
 
+        // Returns the character associated with the node
         public char getChar() { return c; }
+        // Returns the phrase of the node
+        public int getPhrase() { return phrase; }
     }
 
-    public boolean search(char c) {
+    // Returns the current node
+    public Node getCurrNode() { return currNode; }
 
-        for(int i = 0; i < root.children.length; i++) {
-            if(currNode.children[i].getChar() == c) return true;
+    /**
+     * Resets the current node pointer to the root
+     */
+    public void resetCurrNode() { currNode = root; }
+
+    /**
+     * Searches if the current node has a child that has the value of the given char.
+     * @param c the specified value we want to find
+     * @return
+     */
+    public boolean searchCurrNode(char c) {
+        for(int i = 0; i < root.children.length; i++) { // loop that checks all the currNode's children
+            if(currNode.children[i].getChar() == c) {   // if the child has the specified value, set currNode pointer to that child.
+                currNode = currNode.children[i];
+                return true;
+            }
         }
-
         return false;
     }
 
-    public void insert(char c) {
-
+    /**
+     * Inserts a new child/node at the current node.
+     * @param c the specified value we want to insert
+     */
+    public void insertAtCurrNode(char c) {
+        for(int i = 0; i < currNode.children.length; i++) {
+            // checks for the next empty space in currNode's children, and inserts the new node
+            if(currNode.children[i] == null) {
+                currNode.children[i] = new Node(c, newPhrase);
+                newPhrase++;    // increments the value of the new phrase for the next insert
+                break;
+            } 
+        }
     }
 }

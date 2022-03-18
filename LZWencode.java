2de5b5b82ea6ef585a14fc105d8a81db9cef3ef5
d-nguyen7381 <d.nguyen7381@gmail.com>
2022-03-19 12:21:59 +1300

    encoder updated

--- LZWencode.java
+++ LZWencode.java
@@ -45,7 +45,7 @@ public class LZWencode {
         if(hexArr[hexArr.length - 1] != dictionary.getCurrNode().getChar()) {
             dictionary.resetCurrNode();
             dictionary.searchCurrNode(hexArr[hexArr.length - 1]);
-            // System.out.println(dictionary.getCurrNode().getPhrase());
+            System.out.println(dictionary.getCurrNode().getPhrase());
         }
 
     }

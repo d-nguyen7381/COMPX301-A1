2c7442f5af619a8f4b78a1cd5d97b56fa1833162
TikiTikiPlus <nootherthanme65@gmail.com>
2022-03-26 22:06:41 +1300

    removed to string

--- hex2byte.java
+++ hex2byte.java
@@ -20,8 +20,8 @@ public class hex2byte {
         byte[] strBytes = hexToBytes(strHex);   // Converts the hex values into a stream of bytes
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         bos.write(strBytes,0,strBytes.length);
-        String streamData = bos.toString();
-        System.out.println(streamData);
+        //String streamData = bos.toString();
+        System.out.println(bos);
     }
 
     /**
